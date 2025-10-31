9c0051c77170a251d695f59d0f01cde8bdcc98c1
Passianoto1 <gustavopassianotocod@gmail.com>
2025-10-31 16:50:57 -0400

    kkkk

---
+++ dia31/sw.js
@@ -0,0 +1,57 @@
+import {offlineFallback, warmStrategyCache} from 'workbox-recipes'
+import {CacheFirst, StaleWhileRevalidate} from 'workbox-strategies'
+import {registerRoute, Route} from 'workbox-routing'
+import {CacheableResponsePlugin} from 'workbox-cacheable-response'
+import {ExpirationPlugin} from 'workbox-expiration'
+
+// configurando o cahce
+const pageCache = new CacheFirst({
+    cacheName: 'pwa-cam',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200]
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+        })
+    ]
+})
+
+// indicando o cahce de página
+warmStrategyCache({
+    urls: ['/index.html', '/'],
+    strategy: pageCache,
+})
+
+// registrando a rota
+registerRoute(({request}) => request.mode === 'navigate', pageCache)
+
+registerRoute(// configurando cache e assets
+   ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+   new StaleWhileRevalidate({
+    cacheName: 'asset-cahce',
+    plugins: [
+        new CacheableResponsePlugin(
+            {
+                statuses : [0, 200],
+            }),
+    ],
+   }),
+)
+
+offlineFallback({
+    pageFallback: '/offline.html',
+})
+
+const imageRoute = new Route (({request}) => {
+    return request.destination === 'image'
+}, new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+        new ExpirationPlugin({
+            maxAgeSeconds: 60 * 60 * 24 * 30,
+        })
+    ]
+})
+)
+registerRoute(imageRoute)
